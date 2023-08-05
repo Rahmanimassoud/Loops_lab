9537ddfefc7d290f3bc3221cde9a9ec4f1deb50a
Massoud Rahmani <mrahmani@LABA.PSWAS.LOCAL>
2023-08-04 17:15:47 -0700

    Loops_lab

---
+++ loops_lab.js
@@ -0,0 +1,55 @@
+// Write a for loop that logs 0-15;
+
+// for(let i = 0; i<=15; i++){
+//   console.log(i)
+// };
+
+// Write a for loop that counts from 15-0
+// for(let i = 15; i>-1; i--){
+//   console.log(i)
+// }
+
+
+// Make variable laps = 0; write a for loop that logs (“that’s another lap!”) and adds 1 to the laps variable. AFTER the loop, log laps.
+// let laps = 0;
+// for(let i = 0; i < 10; i++){
+//   laps += i;
+//   console.log("Thats is another lap!" + laps)
+// }
+// console.log(laps)
+// THIS IS NOT CORRECT MIND STOPED WORKING;
+
+
+// Make variable age. Make a loop from 0-100. In every loop check if i == age, if so log something, else say “age doesn’t match”.
+// let age  = 29;
+// for(let i = 0; i<101; i++){
+//   if(i == age){
+//     console.log("Happy BD your " + age + " Years old")
+//     // break; we can use this to exit out of the loop no need check once it matches our variable.
+//   } else {
+//     console.log("Age doesn't match")
+//   }
+// }
+
+
+// Write a loop that logs all even numbers from 0-700.
+// for(let i = 0; i<701; i++){
+//   if(i % 2 == 0){
+//     console.log(i)
+//   }
+// };
+
+// for(let i = 0; i<=700; i+= 2){
+//   console.log(i)
+// }
+
+// Write a loop that logs all odd numbers from 0-700.
+// for(let i = 0; i<701; i++){
+//   if(i % 2 !== 0){
+//     console.log(i)
+//   }
+// };
+
+// for(let i = 1; i<=700; i+= 2){
+//   console.log(i)
+// }
