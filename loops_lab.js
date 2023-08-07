0336266d56af565e69b03aa3eab3d8f06801a385
Massoud Rahmani <mrahmani@LABA.PSWAS.LOCAL>
2023-08-07 09:46:32 -0700

    comments

--- loops_lab.js
+++ loops_lab.js
@@ -1,13 +1,13 @@
 // Write a for loop that logs 0-15;
 
-// for(let i = 0; i<=15; i++){
-//   console.log(i)
-// };
+for(let i = 0; i<=15; i++){
+  console.log(i)
+};
 
 // Write a for loop that counts from 15-0
-// for(let i = 15; i>-1; i--){
-//   console.log(i)
-// }
+for(let i = 15; i>-1; i--){
+  console.log(i)
+}
 
 
 // Make variable laps = 0; write a for loop that logs (“that’s another lap!”) and adds 1 to the laps variable. AFTER the loop, log laps.
@@ -17,63 +17,64 @@ for(let i = 0; i < 10; i++){
   console.log("Thats is another lap!" + laps)
 }
 console.log(laps)
-// THIS IS NOT CORRECT MIND STOPED WORKING;
+
 
 
 // Make variable age. Make a loop from 0-100. In every loop check if i == age, if so log something, else say “age doesn’t match”.
-// let age  = 29;
-// for(let i = 0; i<101; i++){
-//   if(i == age){
-//     console.log("Happy BD your " + age + " Years old")
-//     // break; we can use this to exit out of the loop no need check once it matches our variable.
-//   } else {
-//     console.log("Age doesn't match")
-//   }
-// }
+let age  = 29;
+for(let i = 0; i<101; i++){
+  if(i == age){
+    console.log("Happy BD your " + age + " Years old")
+    // break; we can use this to exit out of the loop no need check once it matches our variable.
+  } else {
+    console.log("Age doesn't match")
+  }
+}
 
 
 // Write a loop that logs all even numbers from 0-700.
-// for(let i = 0; i<701; i++){
-//   if(i % 2 == 0){
-//     console.log(i)
-//   }
-// };
+for(let i = 0; i<701; i++){
+  if(i % 2 == 0){
+    console.log(i)
+  }
+};
 
-// for(let i = 0; i<=700; i+= 2){
-//   console.log(i)
-// }
+for(let i = 0; i<=700; i+= 2){
+  console.log(i)
+}
 
 // Write a loop that logs all odd numbers from 0-700.
-// for(let i = 0; i<701; i++){
-//   if(i % 2 !== 0){
-//     console.log(i)
-//   }
-// };
+for(let i = 0; i<701; i++){
+  if(i % 2 !== 0){
+    console.log(i)
+  }
+};
 
-// for(let i = 1; i<=700; i+= 2){
-//   console.log(i)
-// }
+for(let i = 1; i<=700; i+= 2){
+  console.log(i)
+}
 
-// for(let i = 1900; i<1949; i++){
-//   console.log("Happy new year " + " The year is now " + i);
-//   if(i === 1900){
-//     console.log("Happy new year " + " The year is now " + i + " and Zeppelin was invented in this year")
+// Write a loop with some “if else” statements. The loop should start at 1900 and go to 1949 and always log ("happy new year! The year is now ????). You should also log if something was invented that year. For example when i = 1902 the code should log "happy new year! The year is now 1902." AND “the teddy bear is invented!
+for(let i = 1900; i<1949; i++){
+  console.log("Happy new year " + " The year is now " + i);
+  if(i === 1900){
+    console.log("Happy new year " + " The year is now " + i + " and Zeppelin was invented in this year")
     
-//   } else if(i === 1902){
-//     console.log("Happy new year " + " The year is now " + i + " and Teddy Bear was invented in this year")
-//   } else if(i === 1910){
-//     console.log("Happy new year " + " The year is now " + i + " and Talking Motion picture was invented in this year")
-//   } else if(i === 1913){
-//     console.log("Happy new year " + " The year is now " + i + " and Bra was invented in this year")
-//   } else if(i === 1918){
-//     console.log("Happy new year " + " The year is now " + i + " and Fortun Cookie was invented in this year")
-//   } else if(i === 1923){
-//     console.log("Happy new year " + " The year is now " + i + " and Traffic Signal was invented in this year")
-//   } else if(i === 1935){
-//     console.log("Happy new year " + " The year is now " + i + " and Radar was invented in this year")
-//   } else if(i === 1938){
-//     console.log("Happy new year " + " The year is now " + i + " and Ballpoint pen was invented in this year")
-//   } else if(i === 1943){
-//     console.log("Happy new year " + " The year is now " + i + " and Slinky was invented in this year")
-//   }
-// }
+  } else if(i === 1902){
+    console.log("Happy new year " + " The year is now " + i + " and Teddy Bear was invented in this year")
+  } else if(i === 1910){
+    console.log("Happy new year " + " The year is now " + i + " and Talking Motion picture was invented in this year")
+  } else if(i === 1913){
+    console.log("Happy new year " + " The year is now " + i + " and Bra was invented in this year")
+  } else if(i === 1918){
+    console.log("Happy new year " + " The year is now " + i + " and Fortun Cookie was invented in this year")
+  } else if(i === 1923){
+    console.log("Happy new year " + " The year is now " + i + " and Traffic Signal was invented in this year")
+  } else if(i === 1935){
+    console.log("Happy new year " + " The year is now " + i + " and Radar was invented in this year")
+  } else if(i === 1938){
+    console.log("Happy new year " + " The year is now " + i + " and Ballpoint pen was invented in this year")
+  } else if(i === 1943){
+    console.log("Happy new year " + " The year is now " + i + " and Slinky was invented in this year")
+  }
+}
